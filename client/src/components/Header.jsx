54f35a298e7e9afa7bfa682bb51ab305dd791efe
thegittyuser <webdevsserv@gmail.com>
2025-11-19 12:12:34 +0500

    NEw code

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="logo"></div>
+        <nav className="navbar"></nav>
+        <div className="icons"></div>
+      </header>
+    </>
+  );
+}
+export default Header;
